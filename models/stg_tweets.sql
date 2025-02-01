e8cbb4549224fb5e6ec73b506945239102b2bffa
Cheolsoo Park <piaozhexiu@gmail.com>
2025-02-01 22:27:01 +0900

    Extract text from tweet objects

--- models/stg_tweets.sql
+++ models/stg_tweets.sql
@@ -1,14 +1,13 @@
 -- models/stg_tweets.sql
-
-WITH source_data AS (
-    SELECT 
-        get_json_object(raw_data, '$.data') AS raw_data
-    FROM 
-        fast_campus.default.tweet_search
-)
-
-SELECT 
-    explode(from_json(raw_data, 'array<struct<id:string,text:string>>')) AS tweet
-FROM 
-    source_data
+SELECT
+  tweet.text
+FROM
+  (
+    SELECT
+      explode(
+        from_json(data, 'array<struct<id:string, text:string, edit_history_tweet_ids:array<string>>>')
+      ) AS tweet
+    FROM
+      fast_campus.default.tweet_search
+  )
 
