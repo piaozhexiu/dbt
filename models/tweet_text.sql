0bd538ceb7183224238eb07684dc34afb5f3adec
Cheolsoo Park <piaozhexiu@gmail.com>
2025-02-02 22:35:18 +0900

    Add date and hour

--- models/tweet_text.sql
+++ models/tweet_text.sql
@@ -1,8 +1,12 @@
 SELECT
-  tweet.text
+  date,
+  hour,
+  tweet.text as tweet_text
 FROM
   (
     SELECT
+      DATE(_airbyte_extracted_at) as date,
+      HOUR(_airbyte_extracted_at) as hour,
       explode(
         from_json(data, 'array<struct<id:string, text:string, edit_history_tweet_ids:array<string>>>')
       ) AS tweet
